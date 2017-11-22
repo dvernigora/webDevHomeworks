8a006e72cda44a65627bd63bd164e8f9270528d5
dvernigora <dmitriy.vernigora.90@gmail.com>
2017-11-22 16:46:39 +0200

    The up button and smooth transitions are realized.

--- WD_PS3 jQuery/Landing-page_1.1/script.js
+++ WD_PS3 jQuery/Landing-page_1.1/script.js
@@ -1,13 +1,21 @@
 $(document).ready(function () {
-    var top_show = 150;
-    var delay = 500;
+    let topShow = 150;
+    let delay = 1000;
     $(window).scroll(function () {
-        if ($(this).scrollTop() > top_show) $('#topBtn').fadeIn();
-        else $('#topBtn').fadeOut();
+        if ($(this).scrollTop() > topShow) {
+            $('#toTop__Btn').fadeIn();
+        } else {
+            $('#toTop__Btn').fadeOut();
+        }
     });
-    $('#topBtn').click(function () {
-        $('body, html').animate({
-            scrollTop: 0
-        }, delay);
+    $('#toTop__Btn').click(function () {
+        $('body, html').animate({scrollTop: 0}, delay);
+    });
+
+
+    $(".scrollToElem").click(function () {
+        let elementClick = $(this).attr("href");
+        let destination = $(elementClick).offset().top;
+        $('html,body').animate({scrollTop: destination}, delay);
     });
 });
