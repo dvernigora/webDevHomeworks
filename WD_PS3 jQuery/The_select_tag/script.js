104102315eca3f0f8cb7b8c2e76f0a0d4b3a4bfb
dvernigora <dmitriy.vernigora.90@gmail.com>
2017-12-05 13:44:19 +0200

    Corrected minor defects.

--- WD_PS3 jQuery/The_select_tag/script.js
+++ WD_PS3 jQuery/The_select_tag/script.js
@@ -1,22 +1,17 @@
 const ARR_NAMES = ['Michael Johnson', 'Emma Williams', 'Daniel Brown', 'Sophia Bell', 'David Wilson'];
 
 $(document).ready(function () {
+    const containerForDropdown = $('.container-for-dropdown');
+    const wrapperForDefaultOption = $('.wrapper-for-default-option');
+    const wrapperForArrow = $('.wrapper-for-arrow');
+    const arrowIcon = $('.arrow__icon');
+    const panel = $('.panel-for-dropdown');
 
-    var containerForDropdown = $('<div>').addClass('container-for-dropdown');
-    var wrapperForDefaultOption = $('<div>').addClass('wrapper-for-option remove-border');
-    var textOfSelectOption = $('<span>').addClass('text__select-option').text('Select Friend');
-    var wrapperForArrow = $('<div>').addClass('wrapper-for-arrow');
-    var arrowIcon = $('<img>').attr({'class': 'arrow__icon', 'src': 'icons/arrow-down.png'});
-    var panel = $('<div>').addClass('panel-for-dropdown');
-    panel.css('display', 'none');
-
-    containerForDropdown.append(wrapperForDefaultOption.append(textOfSelectOption, wrapperForArrow.append(arrowIcon)));
-
-    for (var i = 0, lengthOfArr = ARR_NAMES.length; i < lengthOfArr; i++) {
-        var wrapperForOption = $('<div>').addClass('wrapper-for-option');
-        var wrapperForIconOption = $('<div>').addClass('wrapper-for-icon__option');
-        var iconOfOption = $('<img>').attr({'class': 'icon__option', 'src': ('icons/' + (i + 1) + '.png')});
-        var textOfOption = $('<span>').addClass('text__option').text(ARR_NAMES[i]);
+    for (let i = 0, lengthOfArr = ARR_NAMES.length; i < lengthOfArr; i++) {
+        let wrapperForOption = $('<div>').addClass('wrapper-for-option');
+        let wrapperForIconOption = $('<div>').addClass('wrapper-for-icon__option');
+        let iconOfOption = $('<img>').attr({'class': 'icon__option', 'src': ('icons/' + (i + 1) + '.png')});
+        let textOfOption = $('<span>').addClass('text__option').text(ARR_NAMES[i]);
         if (i === lengthOfArr - 1) {
             wrapperForOption.addClass('remove-border');
         }
@@ -26,18 +21,17 @@ $(document).ready(function () {
     $('body').append(containerForDropdown);
 
     $('.wrapper-for-option').click(function () {
-        var delay = 500;
-        if (panel.is(":visible")) {
-            var elementClick = $(this).html().split('<div class="wrapper-for-arrow">')[0];
+        let delay = 500;
+        if (panel.is(':visible')) {
+            wrapperForArrow.remove();
+            let elementClick = $(this).html();
             wrapperForDefaultOption.empty();
             wrapperForDefaultOption.append(elementClick, wrapperForArrow.append(arrowIcon));
-            wrapperForDefaultOption.addClass('remove-border');
             panel.slideUp(delay);
+            wrapperForDefaultOption.addClass('remove-border');
         } else {
-            wrapperForDefaultOption.empty();
-            wrapperForDefaultOption.append(textOfSelectOption, wrapperForArrow.append(arrowIcon));
-            $(this).removeClass('remove-border');
             panel.slideDown(delay);
+            wrapperForDefaultOption.removeClass('remove-border');
         }
     });
 });
