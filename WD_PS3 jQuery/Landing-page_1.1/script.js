a0c3492a98f4a7ee29c7758627c879f489f85510
dvernigora <dmitry.vernigora.90@gmail.com>
2017-12-13 17:18:48 +0200

    Fixed 'something dumb', and added minor changes.

--- WD_PS3 jQuery/Landing-page_1.1/script.js
+++ WD_PS3 jQuery/Landing-page_1.1/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
-    let topShow = 150;
-    let delay = 1000;
+    const topShow = 150;
+    const delay = 200;
+    let isScrollToTop = false;
+
     $(window).scroll(function () {
         if ($(this).scrollTop() > topShow) {
             $('#toTop__Btn').fadeIn();
@@ -8,10 +10,13 @@ $(document).ready(function () {
             $('#toTop__Btn').fadeOut();
         }
     });
-    $('#toTop__Btn').click(function () {
-        $('body, html').animate({scrollTop: 0}, delay);
-    });
 
+    $('#toTop__Btn').click(function () {
+        if (!isScrollToTop) {
+            isScrollToTop = true;
+            $('body, html').animate({scrollTop: 0}, delay, function(){isScrollToTop = false;});
+        }
+    });
 
     $(".scrollToElem").click(function () {
         let elementClick = $(this).attr("href");
