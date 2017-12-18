d32080c9491d09348d04bb3ae5dd5ba76501b622
dvernigora <dmitriy.vernigora.90@gmail.com>
2017-12-18 18:11:18 +0200

    Added minor changes

--- WD_ST1 ATM/script.js
+++ WD_ST1 ATM/script.js
@@ -2,7 +2,6 @@ var ATM = {
     is_auth: false,
     current_user: false,
     current_type: false,
-    isLoadCash: false,
     // all cash of ATM
     cash: 2000,
     // all available users
@@ -19,8 +18,8 @@ var ATM = {
             return console.log('you are already logged in as ' + this.current_type + '. Account number: ' +
                 this.current_user.number + '. Log out of current account please.');
         } else {
-            for (var i = 0, length = this.users.length; i < length; i++) {
-                var currentUsr = this.users[i];
+            for (let i = 0, length = this.users.length; i < length; i++) {
+                let currentUsr = this.users[i];
                 if (currentUsr.number === number && currentUsr.pin === pin) {
                     this.is_auth = true;
                     this.current_user = currentUsr;
@@ -51,22 +50,23 @@ var ATM = {
         return console.log('Wrong data. Enter the correct number.');
     },
 
-    notificationOfAuthorization: function (type) {
-        if (!this.isAuthByUserType('user') && !this.isAuthByUserType('admin')) {
+    checkAccessRightsFor: function (type) {
+        if (!this.is_auth) {
             console.log('Enter account number and pin code.');
-            return null;
+            return false;
         } else if (this.isAuthByUserType('admin') && type !== 'admin') {
             console.log('You are authorized as an administrator. This option is available only for users.');
-            return null;
+            return false;
         } else if (this.isAuthByUserType('user') && type !== 'user') {
             console.log('You are authorized as an user. This option is available only for administrator.');
-            return null;
+            return false;
         }
+        return true;
     },
 
     // check current debet
     check: function () {
-        if (!this.isAuthByUserType('user') && !this.isAuthByUserType('admin')) {
+        if (!this.is_auth) {
             return console.log('Enter account number and pin code.');
         }
         this.reportOfLastActions.push({
@@ -77,72 +77,88 @@ var ATM = {
 
     // get cash - available for user only
     getCash: function (amount) {
-        if (this.notificationOfAuthorization('user') === null) {
-            return false;
+        if (!this.checkAccessRightsFor('user')) {
+            return;
         }
-        if (this.isValidNum(amount)) {
-            if (!this.isLoadCash) {
-                amount = Math.abs(amount);
-                if (amount > this.cash) {
-                    if (this.cash === 0) {
-                        return console.log('Sorry, but there is no money left in the ATM.');
-                    }
-                    return console.log('Sorry, but there is not enough money in the ATM. ' +
-                        'Try to withdraw the amount less.');
-                } else if (this.current_user.debet - amount < 0) {
-                    return console.log('There are not enough funds on your debet. ' +
-                        'You have: ' + this.current_user.debet);
-                }
-
-                this.reportOfLastActions.push({
-                    type: this.current_type, number: this.current_user.number, actionType: 'get cash',
-                    sum: amount, cashOfATM: (this.cash - amount)
-                });
-            }
-
-            this.current_user.debet -= amount;
-            console.log('Operation successful. On your debet: ' + this.current_user.debet);
-            this.cash -= amount;
-            return true;
+        if (!this.isValidNum(amount)) {
+            return;
         }
+        if (!this.cash) {
+            return console.log('Sorry, but there is no money left in the ATM.');
+        }
+
+        amount = Math.abs(amount);
+
+        if (amount > this.cash) {
+            console.log('Sorry, but there is not enough money in the ATM. ' +
+                'Try to withdraw the amount less.');
+        } else if (this.current_user.debet - amount < 0) {
+            console.log('There are not enough funds on your debet. ' +
+                'You have: ' + this.current_user.debet);
+        }
+
+        this.current_user.debet -= amount;
+        this.cash -= amount;
+
+        this.reportOfLastActions.push({
+            type: this.current_type, number: this.current_user.number, actionType: 'get cash',
+            sum: amount, cashOfATM: this.cash
+        });
+
+        console.log('Operation successful. On your debet: ' + this.current_user.debet);
     },
 
     // load cash - available for user only
     loadCash: function (amount) {
-        this.isLoadCash = true;
-        amount = Math.abs(amount);
-        if (this.getCash(-amount)) {
-            this.reportOfLastActions.push({
-                type: this.current_type, number: this.current_user.number, actionType: 'load cash', sum: amount,
-                cashOfATM: this.cash
-            });
+        if (!this.checkAccessRightsFor('user')) {
+            return;
         }
-        this.isLoadCash = false;
+        if (!this.isValidNum(amount)) {
+            return;
+        }
+
+        amount = Math.abs(amount);
+        this.current_user.debet += amount;
+        this.cash += amount;
+
+        this.reportOfLastActions.push({
+            type: this.current_type, number: this.current_user.number, actionType: 'load cash', sum: amount,
+            cashOfATM: this.cash
+        });
+
+        console.log('Operation successful. On your debet: ' + this.current_user.debet);
     },
 
     // load cash to ATM - available for admin only - EXTENDED
     load_cash: function (addition) {
-        if (this.notificationOfAuthorization('admin') !== null && this.isValidNum(addition)) {
-            addition = Math.abs(addition);
-            this.cash += addition;
-            this.reportOfLastActions.push({
-                type: this.current_type, number: this.current_user.number, actionType: 'addition cash',
-                sum: addition, cashOfATM: this.cash
-            });
-            console.log('Operation successful. Amount of money in ATM: ' + this.cash);
+        if (!this.checkAccessRightsFor('admin')) {
+            return;
         }
+        if (!this.isValidNum(addition)){
+            return;
+        }
+
+        addition = Math.abs(addition);
+        this.cash += addition;
+        this.reportOfLastActions.push({
+            type: this.current_type, number: this.current_user.number, actionType: 'addition cash',
+            sum: addition, cashOfATM: this.cash
+        });
+
+        console.log('Operation successful. Amount of money in ATM: ' + this.cash);
     },
 
     // get report about cash actions - available for admin only - EXTENDED
     getReport: function () {
-        if (this.notificationOfAuthorization('admin') !== null) {
-            console.table(this.reportOfLastActions);
+        if (!this.checkAccessRightsFor('admin')) {
+            return;
         }
+        console.table(this.reportOfLastActions);
     },
 
     // log out
     logout: function () {
-        if (!this.isAuthByUserType('user') && !this.isAuthByUserType('admin')) {
+        if (!this.is_auth) {
             return console.log('Before logging out, you must log into the system.');
         }
         this.reportOfLastActions.push({
