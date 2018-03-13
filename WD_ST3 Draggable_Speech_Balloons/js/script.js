a4a3c91813410368d3df6a3ebf33eaf445798a7e
dvernigora <dmitry.vernigora.90@gmail.com>
2018-03-14 01:54:08 +0200

    implemented code refactoring

--- WD_ST3 Draggable_Speech_Balloons/js/script.js
+++ WD_ST3 Draggable_Speech_Balloons/js/script.js
@@ -5,9 +5,10 @@ const WRAPPER_IMAGE = $('.wrapper__image');
 
 $(document).ready(function () {
     initialize();
-    react_of_dblclick_on_field();
-    edit_message_bubble_text();
-    react_of_keydown();
+    WRAPPER_IMAGE.on('dblclick', handleFieldDbClick);
+    WRAPPER_IMAGE.on('blur', '.message-bubble__input', inpBlurHandler);
+    editMessageBubbleText();
+    reactOfKeydown();
 });
 
 function initialize() {
@@ -19,60 +20,59 @@ function initialize() {
             let text = item.text;
             let isAddToDB = false;
 
-            let messageBubble = add_message_bubble(xCoord, yCoord, id, text);
-            let currentInput = WRAPPER_IMAGE.find('.message-bubble__input');
-            add_text_to_message_bubble(currentInput, messageBubble, isAddToDB);
-            init_draggable_elem(messageBubble);
+            let msgBubble = addMessageBubble(xCoord, yCoord, id, text);
+            let currentInput = msgBubble.find('.message-bubble__input');
+            addTextToMessageBubble(currentInput, msgBubble, isAddToDB);
+            initDraggableElem(msgBubble);
         }
     });
 
 }
 
-function react_of_dblclick_on_field() {
-    $('body').on('dblclick', ".wrapper__image", function (e) {
-        if (e.target.className !== 'wrapper__image') {
-            return;
-        }
+function handleFieldDbClick(e) {
+    if (e.target.className !== 'wrapper__image') {
+        return;
+    }
+    let pos = $(this).offset();
+    let xCoord = e.pageX - pos.left;
+    let yCoord = e.pageY - pos.top;
 
-        let pos = $(this).offset();
-        let xCoord = e.pageX - pos.left;
-        let yCoord = e.pageY - pos.top;
-
-        let messageBubble = add_message_bubble(xCoord, yCoord);
-        
-        let currentInput = WRAPPER_IMAGE.find('.message-bubble__input');
-        currentInput.focus();
-
-        init_draggable_elem(messageBubble);
-        react_of_message_bubble_on_blur(currentInput, messageBubble);
-    });
+    let msgBubble = addMessageBubble(xCoord, yCoord);
+    
+    let currentInput = msgBubble.find('.message-bubble__input');
+    currentInput.focus();
+    initDraggableElem(msgBubble);
 }
 
-function init_draggable_elem(elem) {
-    elem.draggable({containment: "parent", cursor:"move",
+function initDraggableElem(elem) {
+    elem.draggable({containment: 'parent', cursor:'move',
         stop: function(event,ui){
             let pos = WRAPPER_IMAGE.offset();
             let id = $(this).attr('id');
             let xCoord = ui.offset.left - pos.left;
             let yCoord = ui.offset.top - pos.top;
-            update_coordinate(id, xCoord, yCoord);
+            updateCoordinate(id, xCoord, yCoord);
         }
     });
 }
 
-function react_of_message_bubble_on_blur(currentInput, messageBubble) {
-    currentInput.blur(function() {
-        let inputValue = currentInput[0].value;
-        let isAddToDB = true;
-        if (remove_message_bubble_if_empty(messageBubble, inputValue)) {
-            isAddToDB = false;
+function inpBlurHandler(e) {
+    if (e.target) {
+        const msgBubble = $(e.target.parentElement);
+        const currentInput = $(e.target);
+        const inputValue = e.target.value;
+
+        if (!inputValue) {
+            removeFromDb(msgBubble);
+            msgBubble.remove();
+            return false
         }
-        add_text_to_message_bubble(currentInput, messageBubble, isAddToDB);
-    });
+        addTextToMessageBubble(currentInput, msgBubble, true);
+    }
 }
 
-function edit_message_bubble_text() {
-    WRAPPER_IMAGE.on('dblclick', ".message-bubble", function (e) {
+function editMessageBubbleText() {
+    WRAPPER_IMAGE.on('dblclick', '.message-bubble', function (e) {
         if ($(this).find('input').length < 1) {
             let self = $(this);
             let prevTextElem = self.find('.message-bubble__text');
@@ -83,12 +83,12 @@ function edit_message_bubble_text() {
             input.val(prevText);
             self.append(input);
             self.find('.message-bubble__input').focus();
-            react_of_message_bubble_on_blur(input, self);
+            inpBlurHandler(input, self);
         }
     });
 }
 
-function add_message_bubble(xCoord, yCoord, id, text) {
+function addMessageBubble(xCoord, yCoord, id, text) {
     if (!id) {
         id = $('.message-bubble').last().attr('id');
         if (id === undefined) {
@@ -97,71 +97,68 @@ function add_message_bubble(xCoord, yCoord, id, text) {
         id = 'id_' + (+id.substring(id.indexOf('_') + 1) + 1);
     }
 
-    let messageBubble = $('<div>', {id: id, class: 'message-bubble', onmousedown: 'return false',
+    let msgBubble = $('<div>', {id: id, class: 'message-bubble', onmousedown: 'return false',
     onselectstart: 'return false'});
-    messageBubble.offset({top:yCoord, left:xCoord});
+    msgBubble.offset({top:yCoord, left:xCoord});
     let input = $('<input>', {type: 'text', class: 'message-bubble__input'});
     input.val(text);
-    messageBubble.append(input);
-    WRAPPER_IMAGE.append(messageBubble);
-    return messageBubble;
+    msgBubble.append(input);
+    WRAPPER_IMAGE.append(msgBubble);
+    return msgBubble;
 }
 
-function remove_message_bubble_if_empty(messageBubble, inputValue) {
-    let prevTextElem = messageBubble.find('.message-bubble__text');
-    if (prevTextElem.length < 1 && !inputValue) {
-        messageBubble.remove();
-    }
-}
 
-function react_of_keydown() {
-    $('body').on("keydown", function(e){
-        if ($('.message-bubble__input').is( ":focus" )) {
+function reactOfKeydown() {
+    $('body').on('keydown', function(e){
+        if ($('.message-bubble__input').is( ':focus' )) {
             if (e.keyCode !== 13 && e.keyCode !== 27) {
                 return;
             }
             let input = $(document.activeElement);
-            let messageBubble = input.parent();
-            let inputValue = input[0].value;
+            let msgBubble = input.parent();
+            let inputValue = input.val();
 
             if (e.keyCode === 13) {
                 let isAddToDB = true;
-                if (!add_text_to_message_bubble(input, messageBubble, isAddToDB)) {
-                    let prevTextElem = messageBubble.find('.message-bubble__text');
+                let inputValue = input.val();
+                if (!inputValue) {
+                    let prevTextElem = msgBubble.find('.message-bubble__text');
                     if (prevTextElem.length > 0) {
-                        remove_from_db(messageBubble);
+                        removeFromDb(msgBubble);
                     }
-                    messageBubble.remove();
+                    msgBubble.remove();
+                    return false;
                 }
+                addTextToMessageBubble(input, msgBubble, isAddToDB);
             }
             if (e.keyCode === 27) {
-                let prevTextElem = messageBubble.find('.message-bubble__text');
+                let prevTextElem = msgBubble.find('.message-bubble__text');
                 if (prevTextElem.length < 1) {
-                    messageBubble.remove();
+                    msgBubble.remove();
                     return;
                 }
                 input.remove();
-                let prevText = messageBubble.find('.message-bubble__text');
+                let prevText = msgBubble.find('.message-bubble__text');
                 prevText.attr({style: 'display: block;'});
             }
         }
     });
 }
 
-function add_text_to_message_bubble(input, messageBubble, isAddToDB) {
-    let inputValue = input[0].value;
-    let prevTextElem = messageBubble.find('.message-bubble__text');
+function addTextToMessageBubble(input, msgBubble, isAddToDB) {
+    let inputValue = input.val();
+    let prevTextElem = msgBubble.find('.message-bubble__text');
     prevTextElem.attr({style: 'display: block;'});
     let prevText = prevTextElem.text();
 
     if (!inputValue) {
-        remove_from_db(messageBubble);
-        messageBubble.remove();
+        removeFromDb(msgBubble);
+        msgBubble.remove();
         return false;
     }
 
     if (prevText !== inputValue && isAddToDB) {
-        add_to_db(messageBubble, inputValue);
+        addToDb(msgBubble, inputValue);
     }
 
     if (prevTextElem.length < 1) {
@@ -169,13 +166,13 @@ function add_text_to_message_bubble(input, messageBubble, isAddToDB) {
     }
     prevTextElem.text(inputValue);
     input.remove();
-    messageBubble.append(prevTextElem);
+    msgBubble.append(prevTextElem);
     return true;
 }
 
-function add_to_db(messageBubble, text) {
-    let id = messageBubble.attr('id');
-    let pos = messageBubble.offset();
+function addToDb(msgBubble, text) {
+    let id = msgBubble.attr('id');
+    let pos = msgBubble.offset();
     let posParent = WRAPPER_IMAGE.offset();
     let xOffset = posParent.left;
     let yOffset = posParent.top;
@@ -201,8 +198,8 @@ function add_to_db(messageBubble, text) {
     });
 }
 
-function remove_from_db(messageBubble) {
-    let id = messageBubble.attr('id');
+function removeFromDb(msgBubble) {
+    let id = msgBubble.attr('id');
     $.ajax({
         type: 'POST',
         url: PATH_TO_PHP,
@@ -215,7 +212,7 @@ function remove_from_db(messageBubble) {
     });
 }
 
-function update_coordinate(id, xCoord, yCoord) {
+function updateCoordinate(id, xCoord, yCoord) {
     let data = {
         'action': 'update_coord',
         'id': id, 
